8ccb6cf981435d05905fe8799748108e3054ba7f
z2lai <zhenglai3@gmail.com>
2019-04-14 22:57:30 -0400

    Added basic routing and searching

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,50 @@ const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  request.get(nowPlayingURL, (error, response, movieData) => {
-    
-    res.render('index', {});
+router.get('/', (req, res, next) => {
+  request.get(nowPlayingUrl, (error, response, movieData) => { // movieData is response.body
+    const parsedBody = JSON.parse(response.body);
+    // res.json(parsedBody);
+    res.render('index', {
+      movies: parsedBody.results
+    });
+  }); 
+});
+
+// Get movie details page
+router.get('/movie/:id', (req, res, next) => {
+  // res.json(req.params.id);
+  const movieId = req.params.id;
+  const movieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+  request.get(movieUrl, (error, response, movieData) => {
+    const parsedBody = JSON.parse(movieData);
+    res.render('movie-details', {
+      parsedBody
+    });
   });
-  
+});
+
+// Get movie search results
+router.post('/search', (req, res, next) => {
+  // the urlencoded middleware (express) parses the query string in the urlencoded body sent by the form and creates the body object of (key-value pairs) on req
+  const uriSearchQuery = encodeURI(req.body.movieSearch); //encodes a Uniform Resource Identifier (URI) by replacing each instance of certain characters by one, two, three, or four escape sequences representing the UTF-8 encoding of the character 
+  const category = req.body.category
+  const searchUrl = `${apiBaseUrl}/search/${category}?query=${uriSearchQuery}&api_key=${apiKey}` 
+  request.get(searchUrl, (error, response, movieData) => {
+    let parsedBody = JSON.parse(movieData);
+    if (category === "person") {
+      parsedBody.results = parsedBody.results[0].known_for;
+    }
+    res.render('index', {
+      movies: parsedBody.results
+    })
+  });
 });
 
 module.exports = router;
